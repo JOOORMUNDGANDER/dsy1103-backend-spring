0de6188d2534da17e17a18ec353098a625c184dc
Mathias <mathiasargomedo0@gmail.com>
2025-11-24 17:33:03 -0300

    xd

--- src/main/java/com/level/up/proyecto/spring/demo/config/CorsConfig.java
+++ src/main/java/com/level/up/proyecto/spring/demo/config/CorsConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    @Value("${app.cors.allowed-origins:http://3.19.246.108:3000}")
+    @Value("${app.cors.allowed-origins:http://18.191.241.231:3000}")
     private String allowedOrigins;
 
     @Override
